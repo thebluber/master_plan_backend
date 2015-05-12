62a6068dbc52993a05db94f22ce098d723484a52
Jiayi Zheng <thebluber@gmail.com>
2015-05-12 15:04:11 +0200

    Remove set_deadline method in goal model Update test

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -2,11 +2,5 @@ class Goal < ActiveRecord::Base
   belongs_to :user
   has_many :tasks
   validates :title, :user, presence: true
-  after_create :set_deadline
 
-  private
-  #default deadline should be 1 year after the creation of the goal
-  def set_deadline
-    self.deadline ||= self.created_at.advance(years: 1)
-  end
 end

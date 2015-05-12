78d8a4e6214364bca8cef883eef283c76b63888e
Jiayi Zheng <thebluber@gmail.com>
2015-05-12 13:24:11 +0200

    Update fetch_for in tasks service: Only return not completed tasks

--- app/services/tasks_service.rb
+++ app/services/tasks_service.rb
@@ -2,7 +2,7 @@ class TasksService
   def self.fetch_for user, date=nil, order=:flag
     #date must be a date object
     if date
-      user.tasks.where("created_at < ?", date + 1).order(order).reject{ |task| task.onetime? && task.done?(date) }
+      user.tasks.where("created_at < ?", date + 1).order(order).reject{ |task| task.completed? }
     else
       user.tasks.order(order)
     end

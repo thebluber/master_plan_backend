a904b998c85a4cf8fc614ab4079b00b6af305b44
Jiayi Zheng <thebluber@gmail.com>
2015-05-15 15:32:36 +0200

    Add deletable? method in category model Categories can only be deleted, if it has no task

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,8 @@ class Category < ActiveRecord::Base
   belongs_to :user, inverse_of: :categories
   has_many :tasks
   validates :user, :name, presence: true
+
+  def deletable?
+    self.tasks.empty?
+  end
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -4,4 +4,17 @@ class CategoryTest < ActiveSupport::TestCase
   should validate_presence_of(:name)
   should belong_to(:user)
   should have_many(:tasks)
+
+  context "deletable?" do
+    setup do
+      @category = create :category
+    end
+
+    should "only be deletable if there're no tasks belonging to it" do
+      assert @category.deletable?
+      create :task, category: @category
+      @category.reload
+      assert_not @category.deletable?
+    end
+  end
 end

5dc5ca25440b3da24c89b29598dafce5c2621eb8
Jiayi Zheng <thebluber@gmail.com>
2015-07-14 15:06:11 +0200

    Add goal and tasks in db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,16 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 user = User.create({email: 'user@example.com', password: 'password'})
 
+
 Task.create({user_id: user.id, description: "Do some sport", category_id: user.categories[1].id, flag: 0})
 Task.create({user_id: user.id, description: "Finish the implementation of my awesome project", category_id: user.categories[0].id, flag: 3})
 Task.create({user_id: user.id, description: "Send mom a birthday greeting", category_id: user.categories[1].id, flag: 3})
 Task.create({user_id: user.id, description: "Physiotherapy", category_id: user.categories[2].id, flag: 1})
+
+#with deadline
+Task.create({user_id: user.id, description: "Report", category_id: user.categories[0].id, flag: 3})
+
+#with goal
+goal = Goal.create({user_id: user.id, title: "Finish reading the book", description: "Goals!: How to Get Everything You Want Faster Than You Ever Thought Possible"})
+Task.create({user_id: user.id, description: "Read chapter1 of Goals!", category_id: user.categories[1].id, flag: 3, goal_id: goal.id})
+Task.create({user_id: user.id, description: "Read chapter2 of Goals!", category_id: user.categories[1].id, flag: 3, goal_id: goal.id})

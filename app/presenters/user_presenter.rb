3e1eec6495b0636581640d5a0b4ac7a19e450bd7
Jiayi Zheng <thebluber@gmail.com>
2015-07-15 20:18:45 +0200

    Changes in user presenter:Present user with goals and categories goal with id and title category with id and name This is needed in front-end form for creatin, updating task

--- app/presenters/user_presenter.rb
+++ app/presenters/user_presenter.rb
@@ -4,4 +4,12 @@ require 'roar/json'
 module UserPresenter
   include Roar::JSON
   property :email
+  collection :goals, class: Goal do
+    property :id
+    property :title
+  end
+  collection :categories, class: Category do
+    property :id
+    property :name
+  end
 end
--- test/presenters/user_presenter_test.rb
+++ test/presenters/user_presenter_test.rb
@@ -8,4 +8,14 @@ class UserPresenterTest < ActiveSupport::TestCase
     assert_equal represented_user['email'], user.send('email')
   end
 
+  should "represent the goals and categories" do
+    user = create :user
+    goal = create :goal, user: user
+
+    represented_user = user.extend(UserPresenter).to_hash
+
+    assert_equal represented_user['goals'], [{ "id" => goal.id, "title" => goal.title }]
+    assert_equal represented_user['categories'], [{"id"=>1, "name"=>"work"}, {"id"=>2, "name"=>"personal"}, {"id"=>3, "name"=>"miscellaneous"}]
+  end
+
 end
